20129cc7df6ca5853a71a993965c22a8446256a0
raymond <raymond.chen@25sprout.com>
2023-07-02 16:58:02 +0800

    fix: Move source code to src folder

---
+++ src/db.py
@@ -0,0 +1,15 @@
+import pymongo
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
+
+client = pymongo.MongoClient(
+    host=getenv("MONGO_HOST", default='localhost'),
+    port=int(getenv("MONGO_PORT", default=27017)),
+    username=getenv("MONGO_USERNAME", default='root'),
+    password=getenv("MONGO_PASSWORD", default='root')
+)
+
+MONGO_DATABASE = getenv("MONGO_DATABASE", default="blog")
+
+db = client[MONGO_DATABASE]
